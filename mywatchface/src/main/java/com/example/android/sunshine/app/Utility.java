2a7692819167e84ec12e3538fea83310cbec5964
Sandesh Karanam <sandeshkaranam1@gmail.com>
2016-10-30 15:31:37 +0530

    Corrected a package

--- mywatchface/src/main/java/com/example/android/sunshine/app/Utility.java
+++ mywatchface/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -16,27 +16,27 @@ public class Utility {
         // Based on weather code data found at:
         // http://bugs.openweathermap.org/projects/api/wiki/Weather_Condition_Codes
         if (weatherId >= 200 && weatherId <= 232) {
-            return com.example.mywatchface.R.drawable.ic_storm;
+            return R.drawable.ic_storm;
         } else if (weatherId >= 300 && weatherId <= 321) {
-            return com.example.mywatchface.R.drawable.ic_light_rain;
+            return R.drawable.ic_light_rain;
         } else if (weatherId >= 500 && weatherId <= 504) {
-            return com.example.mywatchface.R.drawable.ic_rain;
+            return R.drawable.ic_rain;
         } else if (weatherId == 511) {
-            return com.example.mywatchface.R.drawable.ic_snow;
+            return R.drawable.ic_snow;
         } else if (weatherId >= 520 && weatherId <= 531) {
-            return com.example.mywatchface.R.drawable.ic_rain;
+            return R.drawable.ic_rain;
         } else if (weatherId >= 600 && weatherId <= 622) {
-            return com.example.mywatchface.R.drawable.ic_snow;
+            return R.drawable.ic_snow;
         } else if (weatherId >= 701 && weatherId <= 761) {
-            return com.example.mywatchface.R.drawable.ic_fog;
+            return R.drawable.ic_fog;
         } else if (weatherId == 761 || weatherId == 781) {
-            return com.example.mywatchface.R.drawable.ic_storm;
+            return R.drawable.ic_storm;
         } else if (weatherId == 800) {
-            return com.example.mywatchface.R.drawable.ic_clear;
+            return R.drawable.ic_clear;
         } else if (weatherId == 801) {
-            return com.example.mywatchface.R.drawable.ic_light_clouds;
+            return R.drawable.ic_light_clouds;
         } else if (weatherId >= 802 && weatherId <= 804) {
-            return com.example.mywatchface.R.drawable.ic_cloudy;
+            return R.drawable.ic_cloudy;
         }
         return -1;
     }
